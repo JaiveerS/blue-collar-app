21176806c1c0751723e2fefbb18b1aab9eb68dd4
JaiveerS <jaiveer_@hotmail.com>
2022-03-14 18:05:11 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+// import Home from './components/Home';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 function App() {
