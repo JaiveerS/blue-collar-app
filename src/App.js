e1c27d87052bbb2d2f65872fe186bf33d002eec2
JaiveerS <jaiveer_@hotmail.com>
2022-03-14 17:58:44 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+      {/* <Home /> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,53 @@
 import React from "react";
-import Logo from "../images/ontreno-logo.svg"
+import Logo from "../images/ontreno-logo.svg";
 import './Navbar.css';
+import {Link} from 'react-router-dom'
+import { Twirl as Hamburger } from 'hamburger-react'
+
 
 export default function Navbar() {
+    const [isOpen, setOpen] = React.useState(false)
+
+    const closeMenu = () => setOpen(false);
+
     return(
-        <div className="nav">
-            <img className="nav--icon" src={Logo} alt="OntReno" ></img>
-        </div>
+        <nav className="navbar">
+            <div className="navbar--container">
+                <a href="/" className="navbar--logo">
+                    <img className="navbar--logo" src={Logo} alt="OntReno" ></img>
+                </a>
+                <div className="menu--icon">
+                    <Hamburger toggled={isOpen} toggle={setOpen} color="black"/>
+                </div>
+                <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav--item">
+                        <Link to='/' className="nav--links" onClick={closeMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link to='/about-us' className="nav--links" onClick={closeMenu}>
+                            About Us
+                        </Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link to='/projects' className="nav--links" onClick={closeMenu}>
+                            Projects
+                        </Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link to='/contact-us' className="nav--links" onClick={closeMenu}>
+                            Contact Us
+                        </Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link to='/call' className="nav--links--mobile" onClick={closeMenu}>
+                            Call
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
