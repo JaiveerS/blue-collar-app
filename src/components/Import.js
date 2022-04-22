f1d8b1ddb4f37f3d8d9896e7a2e1e298bb9aab2c
JaiveerS <jaiveer_@hotmail.com>
2022-04-21 23:31:26 -0400

    fixed errors

--- src/components/Import.js
+++ src/components/Import.js
@@ -9,7 +9,7 @@ export default function Import() {
   const images = importAll(require.context('../images/gallery', false, /\.(png|jpe?g|svg)$/));
 
   const pics = images.map(image =>
-    <Picture src={image}/>
+    <Picture key={image} src={image}/>
   )
 
     return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
 import Images from "./Import";
-import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 export default function Projects() {
